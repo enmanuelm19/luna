80293bb025e00e86653fc8cc67ddd11bf4e8ed4f
Enmanuel <enmanuelm19@gmail.com>
2020-02-23 15:07:13 -0300

    - Removing useless js

--- app/assets/javascripts/spree/backend/all.js
+++ app/assets/javascripts/spree/backend/all.js
@@ -1,24 +1,3 @@
 //= require jquery3
 //= require select2
 //= require spree
-
-function formatState (state) {
-  console.log("state", state)
-  if (!state.id) {
-    return state.text;
-  }
-  var $state = $(
-    '<span style="'+ state.text +'"> ' + state.text + '</span>'
-  );
-  return $state;
-};
-
-function formatSelect () {
-  var selectVariant = document.getElementById('color-variant');
-  if (selectVariant) {
-    console.log("select", selectVariant)
-  }
-  selectVariant.select2({
-    templateResult: formatState
-  });
-}
