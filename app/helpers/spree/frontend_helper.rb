c683d8a4ebdc085815c6475f2503d87b553e49f0
Enmanuel <enmanuelm19@gmail.com>
2020-02-28 19:25:31 -0300

    - Added translations and fixing translation in helper

---
+++ app/helpers/spree/frontend_helper.rb
@@ -0,0 +1,42 @@
+module Spree
+  module FrontendHelper
+    def checkout_progress(numbers: false)
+      states = @order.checkout_steps - ['complete']
+      items = states.each_with_index.map do |state, i|
+        text = Spree.t("order_states.#{state}").titleize
+        text.prepend("#{i.succ}. ") if numbers
+
+        css_classes = ['text-uppercase nav-item']
+        current_index = states.index(@order.state)
+        state_index = states.index(state)
+
+        if state_index < current_index
+          css_classes << 'completed'
+          link_content = content_tag :span, nil, class: 'checkout-progress-steps-image checkout-progress-steps-image--full'
+          link_content << text
+          text = link_to(link_content, spree.checkout_state_path(state), class: 'd-flex flex-column align-items-center', method: :get)
+        end
+
+        css_classes << 'next' if state_index == current_index + 1
+        css_classes << 'active' if state == @order.state
+        css_classes << 'first' if state_index == 0
+        css_classes << 'last' if state_index == states.length - 1
+        # No more joined classes. IE6 is not a target browser.
+        # Hack: Stops <a> being wrapped round previous items twice.
+        if state_index < current_index
+          content_tag('li', text, class: css_classes.join(' '))
+        else
+          link_content = if state == @order.state
+                           content_tag :span, nil, class: 'checkout-progress-steps-image checkout-progress-steps-image--full'
+                         else
+                           inline_svg_tag 'circle.svg', class: 'checkout-progress-steps-image'
+                         end
+          link_content << text
+          content_tag('li', content_tag('a', link_content, class: "d-flex flex-column align-items-center #{'active' if state == @order.state}"), class: css_classes.join(' '))
+        end
+      end
+      content = content_tag('ul', raw(items.join("\n")), class: 'nav justify-content-between checkout-progress-steps', id: "checkout-step-#{@order.state}")
+      content << content_tag('div', raw('<hr /><hr /><hr />'), class: "checkout-progress-steps-line state-#{@order.state}")
+    end
+  end
+end
